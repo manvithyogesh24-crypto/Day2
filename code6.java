b31de15749c0360315b4bc45e9fdeb89e63cf336
Manvith Y <manvithyogesh40@gmail.com>
2026-01-29 15:42:45 +0530

    Add files via upload

---
+++ code6.java
@@ -0,0 +1,11 @@
+class code6 {
+    public static void main(String[] args) {
+        int A = 10;
+
+        if (A % 2 == 0) {
+            System.out.println("The number is Even");
+        } else {
+            System.out.println("The number is Odd");
+        }
+    }
+}
---
+++ code7.java
@@ -0,0 +1,8 @@
+class code7 {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 24;
+        String result = (B > A) ? "True" : "False";
+        System.out.println(result);
+    }
+}
